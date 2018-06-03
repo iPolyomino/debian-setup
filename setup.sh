1431f5a9770e7083dddf4ad18573bbfad53450b3
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2018-06-03 15:51:26 +0900

    first commit

---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install zsh zsh-doc
+
+sudo chsh -s $(which zsh)
+
+sudo apt-get install emacs24 emacs24-el
+cat emacs.txt > ~/.emacs
+
+sudo apt-get install vim
+cat vimrc.txt > ~/.vimrc
+
+sudo apt-get install xsel
+echo 'alias pbcopy="xsel -i -b"' >> ~/.zshrc
+echo 'alias bppaste="xsel -o -b"' >> ~/.zshrc
+
+sudo apt-get install git curl tmux fbterm
+sudo apt-get install golang
+mkdir ~/go
+echo 'export GOPATH="$HOME/go"' >> ~/.zshrc
+echo 'export PATH="$PATH:$HOME/go/bin"' >> ~/.zshrc
+
+source ~/.zshrc
+
+go get -u github.com/junegunn/fzf
+go get -u github.com/motemen/ghq
+
+cat ghq-fzf-setup.txt >> ~/.zshrc
+
+curl https://sh.rustup.rs -sSf | sh
+source $HOME/.cargo/env
+echo 'export PATH="$PATH:$HOME/.cargo/bin"' >> ~/.zshrc
+
+sudo apt-get install xserver-xorg xbase-clients xterm twm rxvt-ml
+
---
+++ vimrc.txt
@@ -0,0 +1,37 @@
+syntax on
+
+" setting
+set fenc=utf-8
+set nobackup
+set noswapfile
+set autoread
+set hidden
+set showcmd
+
+set number
+set cursorline
+set cursorcolumn
+set virtualedit=onemore
+set smartindent
+set visualbell
+set showmatch
+set laststatus=2
+set wildmode=list:longest
+nnoremap j gj
+nnoremap k gk
+
+set list listchars=tab:\▸\-
+set expandtab
+set tabstop=4
+set shiftwidth=4
+
+set ignorecase
+set smartcase
+set incsearch
+set wrapscan
+set hlsearch
+nmap <Esc><Esc> :nohlsearch<CR><Esc>
+
+imap <C-j> <esc>
+
+set backspace=indent,eol,start
